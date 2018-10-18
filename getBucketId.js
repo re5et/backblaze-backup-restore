63d7429a14fb532066aa9205149a34301654cced
re5et <re5etsmyth@gmail.com>
2018-10-18 10:58:41 -0700

    Cache bucketId

--- getBucketId.js
+++ getBucketId.js
@@ -3,8 +3,15 @@ const request = require('request');
 
 const _ = require('lodash');
 
+let bucketId
+
 module.exports = function getBucketId(options, callback){
   const bucketName = process.env['BACKUP_RESTORE_BACKBLAZE_BUCKET'];
+	if (bucketId) {
+		logger.info('using cached bucketId for: ', bucketName);
+		options.bucketId = bucketId;
+		return callback(null, options)
+	}
   logger.info('getting bucketId for: ', bucketName);
   const url = options.apiUrl + '/b2api/v1/b2_list_buckets';
   request({
@@ -19,6 +26,7 @@ module.exports = function getBucketId(options, callback){
   }, function(err, response, body){
     if(err){ throw new Error(err) }
     const bucket = _.find(body.buckets, ['bucketName', bucketName]);
+		bucketId = bucket.bucketId
     options.bucketId = bucket.bucketId;
     callback(err, options);
   });
