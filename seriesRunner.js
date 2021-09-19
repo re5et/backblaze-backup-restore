28cd2198e405c64fd88fc4c2974d3523c754f61e
re5et <re5etsmyth@gmail.com>
2021-09-19 11:33:35 -0700

    Improve series runner

--- seriesRunner.js
+++ seriesRunner.js
@@ -1,10 +1,27 @@
 const async = require('async');
+const _ = require('lodash');
 
+const trackError = require('./trackError')
 const logger = require('./logger');
 
-module.exports = function(tasks) {
-  async.series(tasks, function(err, results){
-    if(err){ return logger.error(err); }
+module.exports = function(tasks, options = {}) {
+  const tasksLength = tasks.length
+  if (options.shuffle) {
+    tasks = _.shuffle(tasks)
+  }
+  async.series(tasks.map((t, i) => {
+    return function(callback) {
+      const taskNumber = i+1
+      const percentComplete = ((taskNumber / tasksLength) * 100).toPrecision(4)
+      logger.info(`seriesRunner starting task ${i+1} of ${tasks.length} (${percentComplete}%) `)
+      return t(callback)
+    }
+  }), function(e, results){
+    if(e){
+      trackError(e)
+      process.exit(1);
+    }
     logger.info('done.');
+    process.exit(0);
   });
 }
